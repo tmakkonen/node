6392777d8c4f248961d21dda10ef2dbc18c42c3f
Tuomo Makkonen <tuomo.makkonen@iki.fi>
2012-02-04 21:53:40 +0100

    my first google map\!

--- client.js
+++ client.js
@@ -1,8 +1,19 @@
 $(document).ready(function() {
   if (navigator.geolocation) {
-    console.log('Geolocation is supported!');
-  }
-  else {
-    console.log('Geolocation is not supported for this Browser/OS version yet.');
+    navigator.geolocation.getCurrentPosition(function(position) {
+        showMap(position.coords.latitude, position.coords.longitude);
+    });
   }
 });
+
+function showMap(lat, lon) {
+    var point = new google.maps.LatLng(lat, lon);
+    var opts = {
+        center : point, 
+        zoom : 15,
+        mapTypeId : google.maps.MapTypeId.ROADMAP
+    };
+
+    var map = new google.maps.Map(document.getElementById("map_canvas"), opts);
+    var mark = new google.maps.Marker({position : point, map : map, title : "BAM"});
+}
